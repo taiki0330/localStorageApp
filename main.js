2d7d8461c94c017e3ebb6ba28dc482517174b6d2
taiki0330 <jonas.taiki.0330@icloud.com>
2023-11-16 00:26:11 +0900

    localstorageに配列で格納

--- main.js
+++ main.js
@@ -20,17 +20,41 @@ const outerBoxIndex = 0;
 const $outerBoxLength = $outerBox.length;
 
 
+// const localStorageInputValue0 = localStorage.getItem("arrayvalue0");
 
+let arrayValue0 = [];
+let arrayValue1 = [];
+let arrayValue2 = [];
+let arrayValue3 = [];
+let arrayValue4 = [];
 
 // 食費データ追加
 $addButton[0].addEventListener('click', (e) => {
-      // e.preventDefault();
-      // console.log($input[0].value);
-      const p = document.createElement('p');
       const inputValue0 = $input[0].value;
-      p.textContent = `- ${inputValue0}円`;
-      $outerBox[0].appendChild(p);
-      localStorage.setItem("0", inputValue0);
+      arrayValue0.push(inputValue0);
+      const jsonArrayValue0 = JSON.stringify(arrayValue0);
+      localStorage.setItem("array0", jsonArrayValue0);
+      // if(localStorageInputValue0 === null) {
+      //       arrayValue0 = [];
+      // } else {
+      //       arrayValue0 = JSON.parse( localStorageInputValue0 )
+      // }
+      // arrayValue0.push(inputValue0);
+      // localStorage.setItem("arrayvalue0", JSON.stringify( arrayValue0 ));
+      // const p = document.createElement('p');
+      // p.textContent = `- ${inputValue0}円`;
+      // $outerBox[0].appendChild(p);
+      // localStorage.setItem("0", inputValue0);
+      const getarray = localStorage.getItem("array0");
+      const jsssonGetArray = JSON.parse(getarray);
+      const numberArr = jsssonGetArray.map(Number);
+      console.log(numberArr);
+      const result = numberArr.reduce(function(sum, element) {
+            return sum + element;
+      },0);
+      console.log(result);
+      console.log(numberArr.slice(-1)[0]);
+      $input[0].value = "";
 })
 let getdata0 = Number(localStorage.getItem("0"));
 $pastButton[0].addEventListener('click', () => {
@@ -41,13 +65,15 @@ $pastButton[0].addEventListener('click', () => {
 
 // 娯楽費データ追加
 $addButton[1].addEventListener('click', (e) => {
-      // e.preventDefault();
-      // console.log($input[0].value);
+      const inputValue1 = $input[1].value;
+      arrayValue1.push(inputValue1);
+      const jsonArrayValue1 = JSON.stringify(arrayValue1);
+      localStorage.setItem("array1", jsonArrayValue1);
       const p = document.createElement('p');
-      const inputValue1 = $input[1].value
       p.textContent = `- ${inputValue1}円`;
       $outerBox[1].appendChild(p);
       localStorage.setItem('1', inputValue1);
+      $input[1].value = "";
 })
 let getdata1 = Number(localStorage.getItem("1"));
 $pastButton[1].addEventListener('click', () => {
@@ -58,13 +84,15 @@ $pastButton[1].addEventListener('click', () => {
 
 // 交通費データ追加
 $addButton[2].addEventListener('click', (e) => {
-      // e.preventDefault();
-      // console.log($input[0].value);
+      const inputValue2 = $input[2].value;
+      arrayValue2.push(inputValue2);
+      const jsonArrayValue2 = JSON.stringify(arrayValue2);
+      localStorage.setItem("array2", jsonArrayValue2);
       const p = document.createElement('p');
-      const inputValue2 = $input[2].value
       p.textContent = `- ${inputValue2}円`;
       $outerBox[2].appendChild(p);
       localStorage.setItem("2", inputValue2);
+      $input[2].value = "";
 })
 let getdata2 = Number(localStorage.getItem("2"));
 $pastButton[2].addEventListener('click', () => {
@@ -75,13 +103,15 @@ $pastButton[2].addEventListener('click', () => {
 
 // 交際費データ追加
 $addButton[3].addEventListener('click', (e) => {
-      // e.preventDefault();
-      // console.log($input[0].value);
+      const inputValue3 = $input[3].value;
+      arrayValue3.push(inputValue3);
+      const jsonArrayValue3 = JSON.stringify(arrayValue3);
+      localStorage.setItem("array3", jsonArrayValue3);
       const p = document.createElement('p');
-      const inputValue3 = $input[3].value
       p.textContent = `- ${inputValue3}円`;
       $outerBox[3].appendChild(p);
       localStorage.setItem("3", inputValue3);
+      $input[3].value = "";
 })
 let getdata3 = Number(localStorage.getItem("3"));
 $pastButton[3].addEventListener('click', () => {
@@ -92,13 +122,15 @@ $pastButton[3].addEventListener('click', () => {
 
 // 日用品データ追加
 $addButton[4].addEventListener('click', (e) => {
-      // e.preventDefault();
-      // console.log($input[0].value);
+      const inputValue4 = $input[4].value;
+      arrayValue4.push(inputValue4);
+      const jsonArrayValue4 = JSON.stringify(arrayValue4);
+      localStorage.setItem("array4", jsonArrayValue4);
       const p = document.createElement('p');
-      const inputValue4 = $input[4].value
       p.textContent = `- ${inputValue4}円`;
       $outerBox[4].appendChild(p);
       localStorage.setItem("4", inputValue4);
+      $input[4].value = "";
 });
 let getdata4 = Number(localStorage.getItem("4"));
 $pastButton[4].addEventListener('click', () => {
@@ -116,3 +148,4 @@ $pastButton[4].addEventListener('click', () => {
 //       p.textContent = $input[inputIndex].value;
 //       $pastButton[pastButtonIndex].appendChild(p);
 // })
+localStorage.clear();
